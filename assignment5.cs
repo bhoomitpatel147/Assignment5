627e979bd92636bf23b5dd6775c58c3e6925e281
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2021-11-11 03:53:38 -0700

    First

---
+++ assignment5.cs
@@ -0,0 +1,7 @@
+public class Character
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Profession { get; set; }
+    public bool IsFictional { get; set; }
+}
